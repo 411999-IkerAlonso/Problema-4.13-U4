965ad820bd91aa62b1d11fe9df796735198de911
Iker <411999@tecnicatura.frc.utn.edu.ar>
2024-10-29 00:54:43 -0300

    No crea personas

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Problema_4._13
+{
+    partial class AMBPersonas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTipoDoc = new System.Windows.Forms.Label();
+            this.lblDocumento = new System.Windows.Forms.Label();
+            this.lblEstadoCivil = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.Fallecido = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.cboTipoDoc = new System.Windows.Forms.ComboBox();
+            this.cboEstadoCivil = new System.Windows.Forms.ComboBox();
+            this.rbtMasculino = new System.Windows.Forms.RadioButton();
+            this.rbtFemenino = new System.Windows.Forms.RadioButton();
+            this.cbFallecido = new System.Windows.Forms.CheckBox();
+            this.lstPersonas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(151, 53);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 0;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(151, 84);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblTipoDoc
+            // 
+            this.lblTipoDoc.AutoSize = true;
+            this.lblTipoDoc.Location = new System.Drawing.Point(114, 116);
+            this.lblTipoDoc.Name = "lblTipoDoc";
+            this.lblTipoDoc.Size = new System.Drawing.Size(86, 13);
+            this.lblTipoDoc.TabIndex = 2;
+            this.lblTipoDoc.Text = "Tipo Documento";
+            // 
+            // lblDocumento
+            // 
+            this.lblDocumento.AutoSize = true;
+            this.lblDocumento.Location = new System.Drawing.Point(133, 156);
+            this.lblDocumento.Name = "lblDocumento";
+            this.lblDocumento.Size = new System.Drawing.Size(62, 13);
+            this.lblDocumento.TabIndex = 3;
+            this.lblDocumento.Text = "Documento";
+            // 
+            // lblEstadoCivil
+            // 
+            this.lblEstadoCivil.AutoSize = true;
+            this.lblEstadoCivil.Location = new System.Drawing.Point(133, 185);
+            this.lblEstadoCivil.Name = "lblEstadoCivil";
+            this.lblEstadoCivil.Size = new System.Drawing.Size(62, 13);
+            this.lblEstadoCivil.TabIndex = 4;
+            this.lblEstadoCivil.Text = "Estado Civil";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(164, 212);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(31, 13);
+            this.lblSexo.TabIndex = 0;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // Fallecido
+            // 
+            this.Fallecido.AutoSize = true;
+            this.Fallecido.Location = new System.Drawing.Point(146, 278);
+            this.Fallecido.Name = "Fallecido";
+            this.Fallecido.Size = new System.Drawing.Size(49, 13);
+            this.Fallecido.TabIndex = 6;
+            this.Fallecido.Text = "Fallecido";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnNuevo.Location = new System.Drawing.Point(37, 387);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(82, 26);
+            this.btnNuevo.TabIndex = 8;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(154, 387);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(82, 26);
+            this.btnEditar.TabIndex = 9;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(279, 387);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(82, 26);
+            this.btnBorrar.TabIndex = 10;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(431, 387);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(82, 26);
+            this.btnGrabar.TabIndex = 11;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(545, 387);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(82, 26);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(659, 387);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(82, 26);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(219, 46);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(114, 20);
+            this.txtApellido.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(219, 77);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(114, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(219, 149);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(114, 20);
+            this.txtDocumento.TabIndex = 3;
+            // 
+            // cboTipoDoc
+            // 
+            this.cboTipoDoc.FormattingEnabled = true;
+            this.cboTipoDoc.Location = new System.Drawing.Point(219, 108);
+            this.cboTipoDoc.Name = "cboTipoDoc";
+            this.cboTipoDoc.Size = new System.Drawing.Size(116, 21);
+            this.cboTipoDoc.TabIndex = 2;
+            // 
+            // cboEstadoCivil
+            // 
+            this.cboEstadoCivil.FormattingEnabled = true;
+            this.cboEstadoCivil.Location = new System.Drawing.Point(219, 177);
+            this.cboEstadoCivil.Name = "cboEstadoCivil";
+            this.cboEstadoCivil.Size = new System.Drawing.Size(116, 21);
+            this.cboEstadoCivil.TabIndex = 4;
+            // 
+            // rbtMasculino
+            // 
+            this.rbtMasculino.AutoSize = true;
+            this.rbtMasculino.Location = new System.Drawing.Point(219, 212);
+            this.rbtMasculino.Name = "rbtMasculino";
+            this.rbtMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbtMasculino.TabIndex = 5;
+            this.rbtMasculino.TabStop = true;
+            this.rbtMasculino.Text = "Masculino";
+            this.rbtMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbtFemenino
+            // 
+            this.rbtFemenino.AutoSize = true;
+            this.rbtFemenino.Location = new System.Drawing.Point(219, 246);
+            this.rbtFemenino.Name = "rbtFemenino";
+            this.rbtFemenino.Size = new System.Drawing.Size(71, 17);
+            this.rbtFemenino.TabIndex = 6;
+            this.rbtFemenino.TabStop = true;
+            this.rbtFemenino.Text = "Femenino";
+            this.rbtFemenino.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.rbtFemenino.UseVisualStyleBackColor = true;
+            // 
+            // cbFallecido
+            // 
+            this.cbFallecido.AutoSize = true;
+            this.cbFallecido.Location = new System.Drawing.Point(219, 278);
+            this.cbFallecido.Name = "cbFallecido";
+            this.cbFallecido.Size = new System.Drawing.Size(15, 14);
+            this.cbFallecido.TabIndex = 7;
+            this.cbFallecido.UseVisualStyleBackColor = true;
+            // 
+            // lstPersonas
+            // 
+            this.lstPersonas.FormattingEnabled = true;
+            this.lstPersonas.Location = new System.Drawing.Point(524, 46);
+            this.lstPersonas.Name = "lstPersonas";
+            this.lstPersonas.Size = new System.Drawing.Size(217, 238);
+            this.lstPersonas.TabIndex = 14;
+            // 
+            // AMBPersonas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 464);
+            this.Controls.Add(this.lstPersonas);
+            this.Controls.Add(this.cbFallecido);
+            this.Controls.Add(this.rbtFemenino);
+            this.Controls.Add(this.rbtMasculino);
+            this.Controls.Add(this.cboEstadoCivil);
+            this.Controls.Add(this.cboTipoDoc);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.Fallecido);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblEstadoCivil);
+            this.Controls.Add(this.lblDocumento);
+            this.Controls.Add(this.lblTipoDoc);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblApellido);
+            this.Name = "AMBPersonas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AMB PERSONAS";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTipoDoc;
+        private System.Windows.Forms.Label lblDocumento;
+        private System.Windows.Forms.Label lblEstadoCivil;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label Fallecido;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.ComboBox cboTipoDoc;
+        private System.Windows.Forms.ComboBox cboEstadoCivil;
+        private System.Windows.Forms.RadioButton rbtMasculino;
+        private System.Windows.Forms.RadioButton rbtFemenino;
+        private System.Windows.Forms.CheckBox cbFallecido;
+        private System.Windows.Forms.ListBox lstPersonas;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Problema_4._13
+{
+    public partial class AMBPersonas : Form
+    {
+        public AMBPersonas()
+        {
+            InitializeComponent();
+            btnGrabar.Enabled = false;
+            btnCancelar.Enabled = false;
+            txtApellido.Enabled = false;
+            txtNombre.Enabled = false;
+            cboEstadoCivil.Enabled = false;
+            cboTipoDoc.Enabled = false;
+            txtDocumento.Enabled = false;
+            rbtFemenino.Enabled = false;
+            rbtMasculino.Enabled = false;
+            cbFallecido.Enabled = false;
+            lstPersonas.Focus();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            btnGrabar.Enabled = true;
+            btnCancelar.Enabled = true;
+            txtApellido.Enabled = true;
+            txtNombre.Enabled = true;
+            cboEstadoCivil.Enabled = true;
+            cboTipoDoc.Enabled = true;
+            txtDocumento.Enabled = true;
+            rbtFemenino.Enabled = true;
+            rbtMasculino.Enabled = true;
+            cbFallecido.Enabled = true;
+            txtApellido.Clear();
+            txtNombre.Clear();
+            cboTipoDoc.SelectedIndex = -1;
+            txtDocumento.Clear();
+            rbtMasculino.Select();
+            cbFallecido.Checked = false;
+            txtApellido.Focus();
+            btnBorrar.Enabled = false;
+            btnEditar.Enabled = false;
+            btnNuevo.Enabled = false;
+            btnSalir.Enabled = false;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            btnGrabar.Enabled = true;
+            btnCancelar.Enabled = true;
+            txtApellido.Enabled = true;
+            txtNombre.Enabled = true;
+            cboEstadoCivil.Enabled = true;
+            cboTipoDoc.Enabled = true;
+            txtDocumento.Enabled = true;
+            rbtFemenino.Enabled = true;
+            rbtMasculino.Enabled = true;
+            cbFallecido.Enabled = true;
+            txtApellido.Clear();
+            txtNombre.Clear();
+            cboTipoDoc.SelectedIndex = -1;
+            txtDocumento.Clear();
+            rbtMasculino.Select();
+            cbFallecido.Checked = false;
+            txtApellido.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult result=MessageBox.Show("¿Desea Salir?", "SALIR", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if(result== DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            btnGrabar.Enabled = false;
+            btnCancelar.Enabled = false;
+            txtApellido.Enabled = false;
+            txtNombre.Enabled = false;
+            cboEstadoCivil.Enabled = false;
+            cboTipoDoc.Enabled = false;
+            txtDocumento.Enabled = false;
+            rbtFemenino.Enabled = false;
+            rbtMasculino.Enabled = false;
+            cbFallecido.Enabled = false;
+            txtApellido.Clear();
+            txtNombre.Clear();
+            cboTipoDoc.SelectedIndex = -1;
+            txtDocumento.Clear();
+            rbtMasculino.Select();
+            cbFallecido.Checked = false;
+            lstPersonas.Focus();
+            btnBorrar.Enabled = true;
+            btnEditar.Enabled = true;
+            btnNuevo.Enabled = true;
+            btnSalir.Enabled = true;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("¿Seguro que desea borrar a la persona?", "BORRAR", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Grabacion Exitosa");
+            btnGrabar.Enabled = false;
+            btnCancelar.Enabled = false;
+            txtApellido.Enabled = false;
+            txtNombre.Enabled = false;
+            cboEstadoCivil.Enabled = false;
+            cboTipoDoc.Enabled = false;
+            txtDocumento.Enabled = false;
+            rbtFemenino.Enabled = false;
+            rbtMasculino.Enabled = false;
+            cbFallecido.Enabled = false;
+            txtApellido.Clear();
+            txtNombre.Clear();
+            cboTipoDoc.SelectedIndex = -1;
+            txtDocumento.Clear();
+            rbtMasculino.Select();
+            cbFallecido.Checked = false;
+            lstPersonas.Focus();
+            btnBorrar.Enabled = true;
+            btnEditar.Enabled = true;
+            btnNuevo.Enabled = true;
+            btnSalir.Enabled = true;
+
+        }
+    }
+}
